dc845418a705b3eae01278054dfc5052848523bc
usuario <usuario@RODRIGO-PC>
2025-09-11 21:31:07 -0300

    Add project files.

---
+++ ProjectoNuevo/Usuario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectoNuevo
+{
+    public class Usuario
+    {
+        public string NombreUsuario { get; set; }
+        public string Password { get; set; }
+        
+        public string RolUsuario { get; set; }
+
+        public Usuario() { }
+        public Usuario(string nombreUsuario, string password, string rolUsuario)
+        {
+            NombreUsuario = nombreUsuario;
+            Password = password;
+            RolUsuario = rolUsuario;
+        }
+    }
+}
